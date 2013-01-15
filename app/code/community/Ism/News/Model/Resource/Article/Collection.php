dd020b5e73afc0a9ad2d0a71f54f4835944f76ac
elena <elena@sergey-laptop.(none)>
2013-01-15 09:31:47 +0200

    news module

---
+++ app/code/community/Ism/News/Model/Resource/Article/Collection.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: e.goshko
+ * Date: 12/26/12
+ * Time: 10:05 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class Ism_News_Model_Resource_Article_Collection extends Mage_Core_Model_Resource_Db_Collection_Abstract {
+
+	public function _construct() {
+		parent::_construct();
+		$this->_init('news/article');
+	}
+
+}
