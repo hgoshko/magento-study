5a3310287fa77af0ba94c1cfc16c8e93b7efe132
Helena <helena.goshko@gmail.com>
2013-01-18 19:11:56 +0200

    deliveryat jquery

---
+++ app/code/community/Ism/DeliveryAt/Model/Observer.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: e.goshko
+ * Date: 1/17/13
+ * Time: 1:50 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class Ism_DeliveryAt_Model_Observer {
+
+	public function addDeliveryat($data)
+	{
+
+	}
+
+}
